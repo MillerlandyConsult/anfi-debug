b2b1325d4e9c28dfd14d67595e298b3ef230e634
Juan Luis Rodríguez Serrano <jrodriguez@mylland.com>
2024-10-26 23:52:57 +0200

    Render variables with JSON encoding
    Previously, variable values were displayed using the @dump directive, which could be difficult to read for complex data structures. This change improves readability by converting variable values to a pretty-printed JSON format with unescaped slashes and unicode characters.

--- resources/views/debug/partials/variables.blade.php
+++ resources/views/debug/partials/variables.blade.php
@@ -1,6 +1,6 @@
 @foreach($variables as $name => $value)
     <div class="variable">
         <h2>{{ $name }}</h2>
-        <pre>@dump($value)</pre>
+        <pre>{{ json_encode($value, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) }}</pre>
     </div>
 @endforeach
