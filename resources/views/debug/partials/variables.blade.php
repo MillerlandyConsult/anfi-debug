939fdb5514f2c2b14d6587afa16a2ee3b6505bed
Agustin Corsetti <acorsetti@mylland.com>
2024-11-06 08:27:50 +0100

    Fix debugged string values

--- resources/views/debug/partials/variables.blade.php
+++ resources/views/debug/partials/variables.blade.php
@@ -1,11 +1,17 @@
 @foreach($variables as $key => $value)
     <div class="key-container">
         <h2>{{ $key }}</h2>
-        @foreach($value as $name => $varValue)
+        @if(!is_array($value))
             <div class="variable-container">
-                <h3>{{ $name }}</h3>
-                <pre>{{ json_encode($varValue, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) }}</pre>
+                <pre>{{ json_encode($value, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) }}</pre>
             </div>
-        @endforeach
+        @else
+            @foreach($value as $name => $varValue)
+                <div class="variable-container">
+                    <h3>{{ $name }}</h3>
+                    <pre>{{ json_encode($varValue, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) }}</pre>
+                </div>
+            @endforeach
+        @endif
     </div>
 @endforeach
