db43cac9af19fc89d8d39d6d561652efb9f7741e
Juan Luis Rodríguez Serrano <jrodriguez@mylland.com>
2025-02-10 11:17:27 +0100

    Disable auto-refresh for debug view.
    Commented out the JavaScript code responsible for periodic data fetching in the debug view. This prevents unnecessary API calls and reduces potential performance overhead during debugging sessions.

--- resources/views/debug/view.blade.php
+++ resources/views/debug/view.blade.php
@@ -47,17 +47,17 @@
     }
 </style>
 <script>
-    setInterval(function () {
-        axios.get('{{ url("/debug/{$key}") }}', {
-            headers: {
-                'X-Requested-With': 'XMLHttpRequest'
-            }
-        })
-            .then(function (response) {
-                console.log(response.data);
-                document.getElementById('variables-container').innerHTML = response.data;
-            });
-    }, 2000);
+    {{--setInterval(function () {--}}
+    {{--    axios.get('{{ url("/debug/{$key}") }}', {--}}
+    {{--        headers: {--}}
+    {{--            'X-Requested-With': 'XMLHttpRequest'--}}
+    {{--        }--}}
+    {{--    })--}}
+    {{--        .then(function (response) {--}}
+    {{--            console.log(response.data);--}}
+    {{--            document.getElementById('variables-container').innerHTML = response.data;--}}
+    {{--        });--}}
+    {{--}, 2000);--}}
 </script>
 </body>
 </html>
